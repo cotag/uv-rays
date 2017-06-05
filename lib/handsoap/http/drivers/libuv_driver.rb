ab8f144d89baf1d72106d90a10ea24d6e5b3d85d
Stephen von Takach <steve@cotag.me>
2017-06-05 16:07:21 +1000

    (hand soap driver) add support for hand soap gem

---
+++ lib/handsoap/http/drivers/libuv_driver.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true, encoding: ASCII-8BIT
+
+module Handsoap
+    module Http
+        module Drivers
+            class LibuvDriver < AbstractDriver
+                def self.load!
+                    require 'uv-rays'
+                end
+
+                def send_http_request_async(request)
+                    endp = ::UV::HttpEndpoint.new(request)
+
+                    if request.username && request.password
+                        request.headers['Authorization'] = [request.username, request.password]
+                    end
+
+                    req = endp.request(request.http_method, {
+                        headers: request.headers,
+                        body: request.body
+                    })
+
+                    deferred = ::Handsoap::Deferred.new
+                    req.then do |resp|
+                        # Downcase headers and convert values to arrays
+                        headers = Hash[resp.map { |k, v| [k.to_s.downcase, Array(v)] }]
+                        http_response = parse_http_part(headers, resp.body, resp.status)
+                        deferred.trigger_callback http_response
+                    end
+                    req.catch do |err|
+                        deferred.trigger_errback err
+                    end
+                    deferred
+                end
+            end
+        end
+    end
+end
