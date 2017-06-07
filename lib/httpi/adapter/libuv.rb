3279dc998c6b7bea3ea0c032dda1c600c0385f68
Stephen von Takach <steve@cotag.me>
2017-06-08 08:51:18 +1000

    (httpi adapter) add client certificate support

--- lib/httpi/adapter/libuv.rb
+++ lib/httpi/adapter/libuv.rb
@@ -21,6 +21,7 @@ class HTTPI::Adapter::Libuv < HTTPI::Adapter::Base
             body: @request.body
         }
 
+        # Apply authentication settings
         auth = @request.auth
         type = auth.type
         if auth.type
@@ -41,6 +42,15 @@ class HTTPI::Adapter::Libuv < HTTPI::Adapter::Base
                     domain: creds[2] || ''
                 }
             end
+        end
+
+        # Apply Client certificates
+        ssl = auth.ssl
+        if ssl.verify_mode == :peer
+            tls_opts = req[:tls_options] = {}
+            tls_opts[:cert_chain]  = ssl.cert.to_pem     if ssl.cert
+            tls_opts[:client_ca]   = ssl.ca_cert_file    if ssl.ca_cert_file
+            tls_opts[:private_key] = ssl.cert_key.to_pem if ssl.cert_key
         end
 
         # Use co-routines to make non-blocking requests
